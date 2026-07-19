dbcd1935469f298087e28f02b266d6b946b543f4
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2026-07-19 01:57:47 -0400

    Implement seeded random number generation for reproducibility and update related tests

--- spec/RandomSpec.js
+++ spec/RandomSpec.js
@@ -5,4 +5,37 @@ describe("Random", function () {
     expect(typeof(number)).toEqual('number');
     expect(number >= 0 && number < 1).toBeTruthy();
   });
+
+  it("same seed produces the same sequence", function () {
+    Random.setSeed(12345);
+    var first = [Random.getNumber(), Random.getNumber(), Random.getNumber()];
+    Random.setSeed(12345);
+    var second = [Random.getNumber(), Random.getNumber(), Random.getNumber()];
+    expect(first).toEqual(second);
+  });
+
+  it("different seeds produce different sequences", function () {
+    Random.setSeed(1);
+    var first = [Random.getNumber(), Random.getNumber(), Random.getNumber()];
+    Random.setSeed(2);
+    var second = [Random.getNumber(), Random.getNumber(), Random.getNumber()];
+    expect(first).not.toEqual(second);
+  });
+
+  it("instances share the global sequence", function () {
+    Random.setSeed(777);
+    var a = new Random().getNumber();
+    Random.setSeed(777);
+    var b = new Random().getNumber();
+    expect(a).toEqual(b);
+  });
+
+  it("#arrayRandomElement is driven by the seed", function () {
+    var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    Random.setSeed(42);
+    var first = [arrayRandomElement(arr), arrayRandomElement(arr), arrayRandomElement(arr)];
+    Random.setSeed(42);
+    var second = [arrayRandomElement(arr), arrayRandomElement(arr), arrayRandomElement(arr)];
+    expect(first).toEqual(second);
+  });
 });
